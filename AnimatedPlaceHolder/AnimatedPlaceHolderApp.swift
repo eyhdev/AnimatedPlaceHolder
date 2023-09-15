c7279add65ce2e1bc5056e1d318f59b8126f646c
eyhdev <alaqsa335@gmail.com>
2023-09-15 15:40:19 +0300

    Initial Commit

---
+++ AnimatedPlaceHolder/AnimatedPlaceHolderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedPlaceHolderApp.swift
+//  AnimatedPlaceHolder
+//
+//  Created by eyh.mac on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedPlaceHolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
